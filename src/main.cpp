bb1e59b9c1ce59740f55eb23d5934c3ab4adf8b2
David Garcia Gonzalez <dgarci23@nd.edu>
2023-04-27 22:19:22 -0400

    Working with ESP32-C3

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@
 
 //MAC Address of the receiver 
 uint8_t broadcastAddress[6];
-uint8_t board_id;
+uint8_t board_id = 1;
 String ssid;
 
 // Battery Status
@@ -51,7 +51,7 @@ void printPerformance() {
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-  esp_sleep_enable_ext0_wakeup(GPIO_NUM_2, MOVEMENT);
+  esp_deep_sleep_enable_gpio_wakeup(1ULL << 4, ESP_GPIO_WAKEUP_GPIO_LOW);
   esp_sleep_enable_timer_wakeup(5*1000000);
   Serial.println("Going to sleep now");
   t_total = millis() - t_total;
@@ -99,6 +99,8 @@ void setup() {
   pinMode(25, OUTPUT);
   digitalWrite(25, HIGH);
 
+  pinMode(4, INPUT_PULLDOWN);
+
   Serial.begin(115200);
   
   initSPIFFS();
@@ -110,35 +112,36 @@ void setup() {
     getBroadcastAddress(broadcastAddress);
     
     if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_TIMER) {
-      float battery_v = (float)analogRead(36)*3.3/4096;
+      float battery_v = (float)analogRead(GPIO_NUM_3)*3.3/4096;
       Serial.printf("Battery Voltage Read: %f\n", battery_v);
       if (battery_v < 1.55 && battery_status) {
         myData.id = board_id;
         myData.battery = 2;
         battery_status = false;
+        Serial.printf("Low Battery\n");
         sendStatus();
       } else if (battery_v > 1.55 && !battery_status) {
         myData.id = board_id;
         myData.battery = 1;
         battery_status = true;
+        Serial.printf("High Battery\n");
         sendStatus();
       } else {
-        esp_sleep_enable_ext0_wakeup(GPIO_NUM_2, MOVEMENT);
+        esp_deep_sleep_enable_gpio_wakeup(1ULL << 4, ESP_GPIO_WAKEUP_GPIO_LOW);
         esp_sleep_enable_timer_wakeup(5*1000000);
         esp_deep_sleep_start();
       }
       return;
     }
 
-    if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT0) {
+    if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_GPIO) {
+      Serial.printf("Sensor Triggered\n");
       myData.id = board_id;
       myData.battery = 0;
       sendStatus();
-
       return;
     }
-
-    esp_sleep_enable_ext0_wakeup(GPIO_NUM_2, MOVEMENT);
+    esp_deep_sleep_enable_gpio_wakeup(1ULL << 4, ESP_GPIO_WAKEUP_GPIO_LOW);
     esp_sleep_enable_timer_wakeup(5*1000000);
     esp_deep_sleep_start();
   }
